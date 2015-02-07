97019184bd8aa073f4cea537fab68d350a18e175
Thomas Breuss <thomasbreuss@gmx.ch>
2015-02-07 08:28:37 +0100

    Merge youtube and vimeo plugin

---
+++ README.md
@@ -0,0 +1,52 @@
+Herbie Video Plugin
+=====================
+
+`Video` ist ein [Herbie](http://github.com/getherbie/herbie) Plugin, mit dem du Videos von 
+[YouTube](http://www.youtube.com) und [Vimeo](https://vimeo.com) in deine Website einbettest.
+
+
+Installation
+-------------
+
+Das Plugin installierst du via Composer.
+
+	$ composer require getherbie/plugin-youtube
+
+Danach aktivierst du das Plugin in der Konfigurationsdatei.
+
+    plugins:
+        enable:
+            - video
+
+
+Anwendung
+---------
+
+Nach der Installation stehen dir die Twig-Funktion `video_youtube` und `video_vimeo` zur Verfügung. Diese rufst du wie folgt auf:
+
+    {{ video_youtube("_0TfPpjDkWU", 480, 320) }}
+    {{ video_vimeo("30459532", 480, 320) }}
+
+Alternativ kannst du die Funktion auch mit benannten Argumenten aufrufen.
+
+    {{ video_youtube(id="", width="480", height="320", responsive="1") }}
+    {{ video_vimeo(id="30459532", width=480, height=320, responsive=1) }}
+
+
+Parameter
+---------
+
+Name        | Beschreibung                          | Typ       | Default
+:---------- | :------------------------------------ | :-------- | :------
+id          | Die ID des Videos                     | string    |  
+width       | Die Breite des Videos in Pixel        | int       | 480
+height      | Die Höhe des Videos in Pixel          | int       | 320
+responsive  | Definiert ob das Video responsiv ist  | bool      | true
+
+
+Demo
+----
+
+<http://getherbie.org/blog/2014/05/09-responsive-youtube-videos>  
+<http://getherbie.org/blog/2014/05/21-vimeo-responsive-videos>
+
---
+++ VideoPlugin.php
@@ -0,0 +1,95 @@
+<?php
+
+/**
+ * This file is part of Herbie.
+ *
+ * (c) Thomas Breuss <www.tebe.ch>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace herbie\plugin\video;
+
+use Herbie;
+use Twig_SimpleFunction;
+
+class VideoPlugin extends Herbie\Plugin
+{
+    /**
+     * @var int
+     */
+    private static $vimeoInstances = 0;
+
+    /**
+     * @var int
+     */
+    private static $youtubeInstances = 0;
+
+    /**
+     * @var Twig_Environment
+     */
+    private $twig;
+
+    public function onTwigInitialized(Herbie\Event $event)
+    {
+        $this->twig = $event['twig'];
+        $this->twig->addFunction(
+            new Twig_SimpleFunction('video_vimeo', [$this, 'vimeo'], ['is_safe' => ['html']])
+        );
+        $this->twig->addFunction(
+            new Twig_SimpleFunction('video_youtube', [$this, 'youtube'], ['is_safe' => ['html']])
+        );
+    }
+
+    /**
+     * @param string $id
+     * @param int $width
+     * @param int $height
+     * @param int $responsive
+     * @return string
+     * @see http://embedresponsively.com/
+     */
+    public function vimeo($id, $width = 480, $height = 320, $responsive = 1)
+    {
+        self::$vimeoInstances++;
+        $template = $this->app['config']->get(
+            'plugins.config.video.template.vimeo',
+            '@plugin/video/templates/vimeo.twig'
+        );
+        return $this->twig->render($template, [
+            'src' => sprintf('//player.vimeo.com/video/%s', $id),
+            'width' => $width,
+            'height' => $height,
+            'responsive' => $responsive,
+            'class' => $responsive ? 'video-vimeo-responsive' : '',
+            'instances' => self::$vimeoInstances
+        ]);
+    }
+
+    /**
+     * @param string $id
+     * @param int $width
+     * @param int $height
+     * @param int $responsive
+     * @return string
+     * @see http://embedresponsively.com/
+     */
+    public function youtube($id, $width = 480, $height = 320, $responsive = 1)
+    {
+        self::$youtubeInstances++;
+        $template = $this->app['config']->get(
+            'plugins.config.video.template.youtube',
+            '@plugin/video/templates/youtube.twig'
+        );
+        return $this->twig->render($template, [
+            'src' => sprintf('//www.youtube.com/embed/%s?rel=0', $id),
+            'width' => $width,
+            'height' => $height,
+            'responsive' => $responsive,
+            'class' => $responsive ? 'video-youtube-responsive' : '',
+            'instances' => self::$youtubeInstances
+        ]);
+    }
+
+}
